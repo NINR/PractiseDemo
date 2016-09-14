2bab8660ae590dbb8eb76bbbee1161a865efecac
NINR <812903508@qq.com>
2016-09-14 12:26:21 +0800

    初始化工程
    初始化

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios,’8.0’
+target ‘PractiseDemo’ do
+pod 'AFNetworking', '~> 3.1.0'
+pod 'Masonry', '~> 1.0.1'
+pod 'SDWebImage', '~> 3.8.2'
+pod 'MJRefresh', '~> 3.1.12'
+end
---
+++ PractiseDemo/MainTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  MainTabBarController.h
+//  PractiseDemo
+//
+//  Created by BearClawr on 16/9/14.
+//  Copyright © 2016年 BearClawr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainTabBarController : UITabBarController
+
+@end
